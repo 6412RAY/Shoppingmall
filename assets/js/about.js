9b7b7a74ec48bc8fb3435507862bfc95a0d7ffa7
6412RAY <92288496+6412RAY@users.noreply.github.com>
2021-12-29 00:46:35 +0800

    Add files via upload

---
+++ assets/js/about.js
@@ -0,0 +1,23 @@
+$(function(){
+    $(".wilsonbox").click(function(){
+        $(".wilson").toggle("slide");
+    });
+});
+
+$(function(){
+    $(".raybox").click(function(){
+        $(".ray").toggle("slide");
+    });
+});
+
+$(function(){
+    $(".mitterbox").click(function(){
+        $(".mitter").toggle("slide");
+    });
+});
+
+$(function(){
+    $(".balldragonbox").click(function(){
+        $(".balldragon").toggle("slide");
+    });
+});
---
+++ assets/js/memformation.js
@@ -0,0 +1,12 @@
+/*表單展開*/
+$(function(){
+    $(".memberbox").click(function(){
+        $(".member").slideToggle("slow");
+    });
+});
+
+$(function(){
+    $(".recordbox").click(function(){
+        $(".record").slideToggle("slow");
+    });
+});
